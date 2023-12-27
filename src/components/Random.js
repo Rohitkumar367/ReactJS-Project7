b660b23da454cc82e7f9e1cef89dcba480f7a568
Rohitkumar367 <rohitddroid@gmail.com>
2023-12-27 15:29:53 +0530

    final uploading using own hook

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
 import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
 
-    const [gif, setGif] = useState("");
-    const [loading, setLoading] = useState(false);
+    // const [gif, setGif] = useState("");
+    // const [loading, setLoading] = useState(false);
     
-    async function fetchData(){
-        setLoading(true);
-        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-        const {data} = await axios.get(url);//-> axios returns a promise
-        const imageSource = data.data.images.downsized_large.url;
-        setGif(imageSource);
-        setLoading(false);
-    }
+    // async function fetchData(){
+    //     setLoading(true);
+    //     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    //     const {data} = await axios.get(url);//-> axios returns a promise
+    //     const imageSource = data.data.images.downsized_large.url;
+    //     setGif(imageSource);
+    //     setLoading(false);
+    // }
 
-    useEffect( () => {
-        fetchData();
-    },[])
+    // useEffect( () => {
+    //     fetchData();
+    // },[])
+
+    const {gif, loading, fetchData} = useGif();
 
 
   return (
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,27 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
 import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Tag = () => {
 
-    const [gif, setGif] = useState("");
-    const [loading, setLoading] = useState(false);
     const [tag, setTag] = useState("car");
-    
-    async function fetchData(){
-        setLoading(true);
-        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
-        const {data} = await axios.get(url);//-> axios returns a promise
-        const imageSource = data.data.images.downsized_large.url;
-        setGif(imageSource);
-        setLoading(false);
-    }
 
-    useEffect( () => {
-        fetchData();
-    },[])
+    // const [gif, setGif] = useState("");
+    // const [loading, setLoading] = useState(false);
+    
+    // async function fetchData(){
+    //     setLoading(true);
+    //     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+    //     const {data} = await axios.get(url);//-> axios returns a promise
+    //     const imageSource = data.data.images.downsized_large.url;
+    //     setGif(imageSource);
+    //     setLoading(false);
+    // }
+
+    // useEffect( () => {
+    //     fetchData();
+    // },[])
+
+    const {gif, loading, fetchData} = useGif(tag);
 
 
   return (
@@ -40,7 +43,7 @@ const Tag = () => {
           value={tag}
         />
 
-        <button onClick={() => fetchData()} className='w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]'>
+        <button onClick={() => fetchData(tag)} className='w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]'>
             Generate
         </button>
       
